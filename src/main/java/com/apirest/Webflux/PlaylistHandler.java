e91cf937047631f95cbf95e365b7b09855bc6d60
Felippe Augusto Alacoque Rodrigues Mariano <felippealacoque@gmail.com>
2020-09-24 22:01:59 -0300

    Criação do service com Endpoint @RestController e estilo reativo

---
+++ src/main/java/com/apirest/Webflux/PlaylistHandler.java
@@ -0,0 +1,43 @@
+package com.apirest.Webflux;
+
+import org.springframework.stereotype.Component;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import static org.springframework.web.reactive.function.server.ServerResponse.ok;
+import static org.springframework.web.reactive.function.BodyInserters.fromPublisher;
+
+import org.springframework.web.reactive.function.server.ServerRequest;
+import org.springframework.web.reactive.function.server.ServerResponse;
+
+import com.apirest.Webflux.document.Playlist;
+import com.apirest.Webflux.service.PlaylistService;
+
+import reactor.core.publisher.Mono;
+
+@Component
+public class PlaylistHandler {
+	
+	@Autowired
+	PlaylistService playlistService;
+	
+	public Mono<ServerResponse> findAll(ServerRequest request){
+		return ok()
+				.contentType(MediaType.APPLICATION_JSON)
+				.body(playlistService.findAll(), Playlist.class);
+	}
+	
+	public Mono<ServerResponse> findById(ServerRequest request){
+		String id = request.pathVariable("id");
+		return ok()
+				.contentType(MediaType.APPLICATION_JSON)
+				.body(playlistService.findById(id), Playlist.class);
+	}
+	
+	public Mono<ServerResponse> save(ServerRequest request){
+		final Mono<Playlist> playlist = request.bodyToMono(Playlist.class);
+		return ok()
+				.contentType(MediaType.APPLICATION_JSON)
+				.body(fromPublisher(playlist.flatMap(playlistService::save), Playlist.class));
+	}
+
+}
---
+++ src/main/java/com/apirest/Webflux/controller/PlaylistController.java
@@ -0,0 +1,75 @@
+package com.apirest.Webflux.controller;
+
+import java.time.LocalDateTime;
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.apirest.Webflux.document.Playlist;
+import com.apirest.Webflux.service.PlaylistService;
+
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.util.function.Tuple2;
+
+@CrossOrigin(origins = "*")
+//@RestController
+public class PlaylistController {
+	
+	@Autowired
+	PlaylistService playlistService;
+	
+	@GetMapping(value="/playlist")
+	public Flux<Playlist> getPlaylist(){
+		return playlistService.findAll();
+	}
+	
+	
+	@GetMapping(value="/playlist/{id}")
+	public Mono<Playlist> getPlaylistId(@PathVariable String id){
+		return playlistService.findById(id);
+	}
+	
+	@PostMapping(value="/playlist")
+	public Mono<Playlist> save(@RequestBody Playlist playlist){
+		return playlistService.save(playlist);
+	}
+	
+	@GetMapping(value="/playlist/webflux", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+	public Flux<Tuple2<Long, Playlist>> getPlaylistByWebflux(){
+
+		System.out.println("---Start get Playlists by WEBFLUX--- " + LocalDateTime.now());
+		Flux<Long> interval = Flux.interval(Duration.ofSeconds(10));
+        Flux<Playlist> playlistFlux = playlistService.findAll();
+
+        return Flux.zip(interval, playlistFlux);
+        
+	}
+
+	@GetMapping(value="/playlist/mvc")
+	public List<String> getPlaylistByMvc() throws InterruptedException {
+
+		System.out.println("---Start get Playlists by MVC--- " + LocalDateTime.now());
+
+		List<String> playlistList = new ArrayList<>();
+		playlistList.add("Java 8");
+		playlistList.add("Spring Security");
+		playlistList.add("Github");
+		playlistList.add("Deploy de uma aplicação java no IBM Cloud");
+		playlistList.add("Bean no Spring Framework");
+		TimeUnit.SECONDS.sleep(15);
+
+		return playlistList;
+
+	}
+}
